14097f02536796ce1c2958151de817dea1a632d1
Vaibhav Saraf <vaibhav.saraf29@gmail.com>
2021-03-20 00:03:29 +0530

    dropdown error

--- inputFieldScript.js
+++ inputFieldScript.js
@@ -4,27 +4,43 @@ var postalCode = document.form1.toPostalCode;
 var postalCodeLen = postalCode.value.length;
 var additionalInfo = document.form1.textarea;
 var additionalInfoLen = additionalInfo.value.length;
+var goingTo = document.form1.goingTo;
+var goingToValue = goingTo.value;
+
 document.getElementById("toPostalCode").classList.remove("error");
 document.getElementById("toPostalCode").setAttribute("aria-invalid", "false");
 document.getElementById("errMsg2").innerHTML = "";
 
 document.getElementById("errMsgTA").innerHTML = "";
-document.getElementById("textarea-maxCharacter").setAttribute("aria-invalid", "false");
-document.getElementById("textarea-maxCharacter").classList.remove("error");
+document.getElementById("textarea").setAttribute("aria-invalid", "false");
+document.getElementById("textarea").classList.remove("error");
 
+document.getElementById("errMsgDD").innerHTML = "";
+document.getElementById("goingTo").setAttribute("aria-invalid", "false");
+document.getElementById("goingTo").classList.remove("error");
+
+
+
+if(goingToValue <= 0) {
+document.getElementById("errMsgDD").innerHTML = "Please select a country";
+document.getElementById("goingTo").setAttribute("aria-invalid", "true");
+document.getElementById("goingTo").classList.add("error");
+
+errors.push(1);
+}
 
 if(postalCodeLen < 7) {
 
 document.getElementById("toPostalCode").classList.add("error");
 document.getElementById("toPostalCode").setAttribute("aria-invalid", "true");
 document.getElementById("errMsg2").innerHTML = "Please enter a valid postal code.";
-errors.push(1);
+errors.push(2);
 
 }
 
 if(additionalInfoLen < 1) {
 
-errors.push(2);
+errors.push(3);
 document.getElementById("errMsgTA").innerHTML = "Please enter a description";
 document.getElementById("textarea").setAttribute("aria-invalid", "true");
 document.getElementById("textarea").classList.add("error");
@@ -32,6 +48,8 @@ document.getElementById("textarea").classList.add("error");
 if(errors.length > 0) {
 ;
 if(errors[0] == 1) {
+document.getElementById("goingTo").focus(); 
+} else if(errors[0] == 2) {
 document.getElementById("toPostalCode").focus();
 
 } else {
